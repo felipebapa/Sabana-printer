da90ae88d93ff10574debe6178abdc332cfcb4a8
alejandrocp15 <126806182+alejandrocp15@users.noreply.github.com>
2023-05-30 10:17:07 -0500

    Update PID_TermistorFinal.ino

--- PID_TermistorFinal.ino
+++ PID_TermistorFinal.ino
@@ -1,48 +1,40 @@
+#include <PIDController.hpp>  // Incluye la biblioteca PIDController.hpp
 
-#include <PIDController.hpp>
+const int PIN_OUTPUT = 6;  // Define el número del pin de salida
 
-const int PIN_INPUT = 0;
-const int PIN_OUTPUT = 6;
+PID::PIDParameters<double> parameters(0.48577, 0.01753, 0);  // Parámetros del controlador PID
 
-PID::PIDParameters<double> parameters(0.4818, 0.01843, 0);
-PID::PIDController<double> pidController(parameters);
+PID::PIDController<double> pidController(parameters);  // Crea una instancia del controlador PID
 
+int Vo;  // Variable para almacenar el valor leído del pin analógico A0
 
-int Vo;
-float R1 = 100000;              // resistencia fija del divisor de tension 
+float R1 = 100000;  // Resistencia fija del divisor de tensión
 float logR2, R2, TEMPERATURA;
-float c1 = 0.3555874631e-03, c2 = 2.649451274e-04, c3 = -0.3398704380e-07;
-// coeficientes de S-H en pagina: 
-// http://www.thinksrs.com/downloads/programs/Therm%20Calc/NTCCalibrator/NTCcalculator.htm
- 
-
+float c1 = 0.3555874631e-03, c2 = 2.649451274e-04, c3 = -0.3398704380e-07;  // Coeficientes para la ecuación S-H
 
 void setup() {
-  
-  
-  pidController.Input = analogRead(PIN_INPUT);
-  pidController.Setpoint = 1100;
-
-  pidController.TurnOn();
-  Serial.begin(9600);		// inicializa comunicacion serie a 9600 bps
+  pidController.Setpoint = 150;  // Establece el valor de consigna del controlador PID
+  pidController.TurnOn();  // Enciende el controlador PID
+  Serial.begin(9600);  // Inicializa la comunicación serie a 9600 bps
 }
 
 void loop() {
-
-
-  pidController.Input = analogRead(PIN_INPUT);
-  pidController.Update();
-
-  analogWrite(PIN_OUTPUT, pidController.Output);
-  Vo = analogRead(A0);			// lectura de A0
-  R2 = R1 * (1023.0 / (float)Vo - 1.0);	// conversion de tension a resistencia
-  logR2 = log(R2);			// logaritmo de R2 necesario para ecuacion
-  TEMPERATURA = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2)); 	// ecuacion S-H
-  TEMPERATURA = TEMPERATURA - 273.15;   // Kelvin a Centigrados (Celsius)
-  TEMPERATURA = (TEMPERATURA + 9*TEMPERATURA)/10;
-  Serial.print("Temperatura: "); 	// imprime valor en monitor serie
+  Vo = analogRead(A0);  // Lee el valor analógico del pin A0
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);  // Convierte la tensión a resistencia
+  logR2 = log(R2);  // Calcula el logaritmo de R2
+  TEMPERATURA = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));  // Aplica la ecuación S-H para obtener la temperatura
+  TEMPERATURA = TEMPERATURA - 273.15;  // Convierte la temperatura de Kelvin a Celsius
+  TEMPERATURA = (TEMPERATURA + 9*TEMPERATURA)/10;  // Suaviza la lectura de la temperatura
+  
+  pidController.Input = TEMPERATURA;  // Establece el valor de entrada del controlador PID
+  pidController.Update();  // Actualiza el controlador PID
+  
+  analogWrite(PIN_OUTPUT, pidController.Output);  // Escribe la salida del controlador PID en el pin de salida
+  
+  Serial.print("Temperatura: ");  // Imprime el valor de la temperatura en el monitor serie
   Serial.print(TEMPERATURA);
   Serial.println(" C"); 
-  delay(100);				// demora de medio segundo entre lecturas
-  Serial.println(pidController.Output);
+
+  delay(300);  // Demora de medio segundo entre lecturas
+  Serial.println(pidController.Output);  // Imprime la salida del controlador PID en el monitor serie
 }
