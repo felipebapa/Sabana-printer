b81b7996141701d42c3608eff8017c51b5c9d96d
alejandrocp15 <126806182+alejandrocp15@users.noreply.github.com>
2023-05-31 12:48:50 -0500

    Update MotorPasoAPasoFinal.ino

--- MotorPasoAPasoFinal.ino
+++ MotorPasoAPasoFinal.ino
@@ -1,10 +1,10 @@
 #include <Stepper.h>  // Incluye la biblioteca Stepper, que proporciona funciones para controlar motores paso a paso.
 
-const int stepsPerRevolution = 48;  // Define la cantidad de pasos por revolución del motor paso a paso.
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);  // Crea una instancia del objeto Stepper llamado "myStepper" con el número de pasos por revolución y los pines de control del motor.
+const int stepsPerRevolution = 39;  // Define la cantidad de pasos por revolución del motor paso a paso.
+Stepper myStepper(stepsPerRevolution, 6, 7, 8, 9);  // Crea objeto Stepper llamado "myStepper" con el número de pasos y los pines de control del motor.
 
 void setup() {
-  myStepper.setSpeed(9);  // Establece la velocidad del motor a 9 RPM.
+  myStepper.setSpeed(4);  // Establece la velocidad del motor a 9 RPM.
 
   Serial.begin(9600);  // Inicializa la comunicación serial con una velocidad de baudios de 9600.
 }
